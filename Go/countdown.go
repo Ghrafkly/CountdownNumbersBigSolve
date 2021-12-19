dde29796c395ecf95cfb68683aaddb1f446762a7
Ghrafkly <69286273+Ghrafkly@users.noreply.github.com>
2021-12-19 21:54:35 +1100

    Step 0 completed

--- Go/countdown.go
+++ Go/countdown.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"fmt"
-	// "sort"
+	"strings"
+
 	"github.com/ernestosuarez/itertools"
 )
 
 func main() {
 	// var ops = [4]string{"+", "-", "*", "/"}
-	var number = []int{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100}
-	var s [][]int
-	allKeys := make(map[[2]int]bool)
+	// var number = []int{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100}
+	var number = []string{"1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10", "25", "50", "75", "100"}
+	var s [][]string
 
-	list := itertools.CombinationsInt(number, 6)
+	allKeys := make(map[string]bool)
+
+	list := itertools.CombinationsStr(number, 4)
 	for item := range list {
-		fmt.Printf("%v\n", item)
-		if _, value := allKeys[item]; !value {
-			allKeys[item] = true
-			s = append(s, [][]int{item}...)
-		}
-	}
+		var strSlc string = strings.Join(item[:], " ")
 
+		if _, value := allKeys[strSlc]; !value {
+			allKeys[strSlc] = true
+			s = append(s, []string{strSlc})
+		}
+
+	}
+	fmt.Printf("S: %v", s)
+	
+	// fmt.Printf("I: %v, J: %v", i, j)
 	// var dupe = duplicate(s)
 	// fmt.Printf("S: %v\n", len(s))
 	// fmt.Printf("D: %v\n", len(dupe))

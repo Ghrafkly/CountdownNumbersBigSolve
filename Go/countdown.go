44138f69f2108656adc72b205eb7f04eb5eb03a3
Ghrafkly <69286273+Ghrafkly@users.noreply.github.com>
2021-12-19 22:06:42 +1100

    3 digit dict and duplicates handling done

--- Go/countdown.go
+++ Go/countdown.go
@@ -8,28 +8,25 @@ import (
 )
 
 func main() {
-	// var ops = [4]string{"+", "-", "*", "/"}
-	// var number = []int{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100}
 	var number = []string{"1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10", "25", "50", "75", "100"}
+	// var ops = [4]string{"+", "-", "*", "/"}
+
+	// Creates a dict of all 3 digit nums
+	threeDigits := make(map[int]int)
+	for i := 101; i < 1000; i++ {
+		threeDigits[i] = 0
+	}
+
+	// Deals with duplicates
 	var s [][]string
-
 	allKeys := make(map[string]bool)
-
-	list := itertools.CombinationsStr(number, 4)
+	list := itertools.CombinationsStr(number, 3)
 	for item := range list {
 		var strSlc string = strings.Join(item[:], " ")
-
 		if _, value := allKeys[strSlc]; !value {
 			allKeys[strSlc] = true
 			s = append(s, []string{strSlc})
 		}
-
 	}
-	fmt.Printf("S: %v", s)
-	
-	// fmt.Printf("I: %v, J: %v", i, j)
-	// var dupe = duplicate(s)
-	// fmt.Printf("S: %v\n", len(s))
-	// fmt.Printf("D: %v\n", len(dupe))
-
+	fmt.Printf("S: %T\n", s)
 }

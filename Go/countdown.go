3c2ab3570080cde6afac6d7b5e3e3ab4ec64f0ee
Ghrafkly <69286273+Ghrafkly@users.noreply.github.com>
2022-01-02 11:14:34 +1100

    Almost done

--- Go/countdown.go
+++ Go/countdown.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
 	"strings"
+	"time"
 
-	// https://pkg.go.dev/github.com/karrick/gorpn#ErrBadBindingType
-	// https://github.com/karrick/gorpn
 	"github.com/ernestosuarez/itertools"
+	"github.com/karrick/gorpn"
 )
 
 var equations [][]string
 
 func main() {
+	start := time.Now()
 	var operators = []string{"+", "-", "*", "/"}
-	// var number = []string{"1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10", "25", "50", "75", "100"}
-	var number = []string{"5", "10", "25"}
+	var number = []string{"1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10", "25", "50", "75", "100"}
 	var ops_needed int = -1
+	var sol []string
 
 	// Creates a dict of all 3 digit numbers. Values set to 0
 	threeDigits := make(map[int]int)
@@ -26,7 +29,7 @@ func main() {
 	// Deals with duplicates for Combinations
 	var cNums [][]string
 	allKeys := make(map[string]bool)
-	comb := itertools.CombinationsStr(number, 3)
+	comb := itertools.CombinationsStr(number, 4)
 
 	for item := range comb {
 		var strSlc string = strings.Join(item[:], ",") // Turns slice into string to create unique key
@@ -52,15 +55,39 @@ func main() {
 		for _, item := range pNums {
 			var current []string
 			rpn(item, operators, current, ops_needed)
-			fmt.Printf("Equations: %v\n", equations)
 		}
+		sol = append(sol, []string(equate(equations))...)
+		equations = nil
 	}
+
+	for _, v := range sol {
+		i, _ := strconv.Atoi(v)
+		threeDigits[i] += 1
+	}
+
+	var keys []int
+	for k := range threeDigits {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		fmt.Printf("Key: %v, Value: %v\n", k, threeDigits[k])
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("Elapsed: %v", elapsed)
+
+	// file, err := os.Open("GoNumbers.csv")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
 }
 
 func rpn(nums []string, ops []string, current []string, ops_needed int) {
 	if ops_needed == 0 && len(nums) == 0 {
-		fmt.Printf("Current: %v\n", current)
-		equations = append(equations, [][]string{current}...)
+		c := make([]string, len(current))
+		copy(c, current)
+		equations = append(equations, [][]string{c}...)
 	}
 
 	if ops_needed > 0 {
@@ -72,15 +99,56 @@ func rpn(nums []string, ops []string, current []string, ops_needed int) {
 	}
 
 	if len(nums) > 0 {
-		v := nums[len(nums)-1]
-		nums = nums[:len(nums)-1]
-		current = append(current, v)
+		v := nums[len(nums)-1]       // Save pop value
+		nums = nums[:len(nums)-1]    // Pop last element
+		current = append(current, v) // Push element
 		rpn(nums, ops, current, ops_needed+1)
-		current = current[:len(current)-1]
+		current = current[:len(current)-1] // These are reached
 		nums = append(nums, v)
 	}
 }
 
-// a := nums[len(nums)-1]  // Save pop value
-// b := nums[:len(nums)-1] // Pop last element
-// b = append(b, a) // Push element
+func equate(equations [][]string) []string {
+	var listResult []string
+	for _, item := range equations {
+		var eq []string
+		var temp []string
+		for _, term := range item {
+			_, err := strconv.Atoi(term)
+			if err == nil {
+				eq = append(eq, term)
+			} else {
+				temp = append(temp, eq[len(eq)-2], eq[len(eq)-1], term)
+				eq = eq[:len(eq)-2]
+
+				var calc string = strings.Join(temp, ",")
+
+				expression, err := gorpn.New(calc)
+				if err != nil {
+					panic(err)
+				}
+
+				result, err := expression.Evaluate(nil)
+				if err != nil {
+					panic(err)
+				} else {
+					rs := fmt.Sprint(result)
+					if isIntegral(result) && result > 0 {
+						if result > 100 && result < 1000 {
+							listResult = append(listResult, []string{rs}...)
+						}
+						eq = append(eq, rs)
+					} else {
+						break
+					}
+				}
+				temp = nil
+			}
+		}
+	}
+	return listResult
+}
+
+func isIntegral(val float64) bool {
+	return val == float64(int(val))
+}
--- Go/go.mod
+++ Go/go.mod
@@ -4,5 +4,5 @@ go 1.17
 
 require (
 	github.com/ernestosuarez/itertools v0.0.0-20190516153236-40a02c159e7b
-	github.com/gammazero/deque v0.1.0
+	github.com/karrick/gorpn v1.4.0
 )
